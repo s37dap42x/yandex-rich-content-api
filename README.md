aed3d38aacf9aefe510ed59ab6b3a6be359120f5
Alex Black <s37dap42x@gmail.com>
2015-11-05 18:12:58 +0300

    Next stage

--- README.md
+++ README.md
@@ -2,17 +2,29 @@
 
 Client for Yandex Rich Content API
 
-Rich Content API предоставляет доступ к контентной системе Яндекса. В ней хранятся десятки миллиардов страниц, и для любой из них можно получить сниппет, заголовок и URL в каноническом виде, полный текст, а также список ссылок на размещённые изображения и видео.
+The Rich Content API provides access to the Yandex content system, containing tens of billions of pages. For any of them, you can receive the snippet, title and URL in canonical form, as well as the complete text and also a list of links to the images and videos they contain. This API is useful for services whose users exchange links. It allows them to see a preview of any web page without leaving the service.
 
-https://tech.yandex.ru/rca/
+[API reference](https://tech.yandex.com/rca/)
+
+# Versioning
+
+Package version corresponds to the version of the API.
 
 # Installation
 
-test
+Add into your `composer.json`:
+
+```json
+{
+  "require": {
+    "s37dap42x/yandex-rich-content-api": "1.0.x"
+  }
+}
+```
 
 # Usage
 
-test
+[Get your own api key](https://tech.yandex.com/keys/get/?service=rca)
 
 #License
 
--- src/RichContent.php
+++ src/RichContent.php
@@ -1,13 +1,99 @@
 <?php namespace Yandex\RichContentAPI;
 
-/**
-* 
-*/
+
 class RichContent
 {
-	
-	function __construct($key)
+    /**
+     * Base URL to Yandex Rich Contetn API
+     */
+	const BASE_API_URL = "http://rca.yandex.com/";
+
+	/**
+     * Unique API key
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * The URL that data is being requested for
+     *
+     * @var string
+     */
+	public $url;
+
+    /**
+     * Optional parameters
+     * See documentation
+     * @link https://tech.yandex.com/rca/doc/dg/index-docpage/
+     *
+     * @var array
+     */
+    public $options = [];
+
+
+    /**
+     * Get a free API key on this page:
+     * @link https://tech.yandex.com/keys/get/?service=rca
+     *
+     * @param string $key The API key
+     */
+	public function __construct($key)
 	{
-		# code...
+		$this->key = $key;
 	}
+
+
+	public function getContent($url)
+	{
+        $this->url = $url;
+
+        $exec_url = $this->composeExecURL();
+        $data = $this->executeData($exec_url);
+
+        return $data;
+	}
+
+    /**
+     * Compose URL for executing
+     *
+     * @return string
+     */
+    public function composeExecURL()
+    {
+        $url = self::BASE_API_URL . "?key=" . $this->key . "&url=" . urlencode($this->url);
+
+        if(!empty($this->options)) {
+            foreach($this->options as $key => $value) {
+                $url .= "&" . $key . "=" . $value;
+            }
+        }
+
+        return $url;
+    }
+
+
+    /**
+     * Execute data from API
+     *
+     * @param string
+     * @return mixed
+     */
+    protected function executeData($url)
+    {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $json = curl_exec($curl);
+
+        curl_close($curl);
+        return $json;
+        /*
+        $data = json_decode($json, true);
+
+        return $data;*/
+    }
+
 }
